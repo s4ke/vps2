473c1bad8fd05310f9a2384c08d38c5c3cdb522a
Georg Rollinger <georg.rollinger@web.de>
2013-04-23 11:02:23 +0200

    MPI_Send test

--- vps2-1/vps2-1.cpp
+++ vps2-1/vps2-1.cpp
@@ -4,12 +4,16 @@
 #include "stdafx.h"
 #include <mpi.h>
 #include <iostream>
+#include <Windows.h>
+
 
 int _tmain(int argc, char* argv[])
 {
 	using namespace std;
 
-	int n, myid, numprocs;
+	int test = 1;
+
+	int myid, numprocs;
     double startwtime = 0.0, endwtime;
     int  namelen;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -22,16 +26,23 @@ int _tmain(int argc, char* argv[])
     cout << "Process " << myid << " of " << numprocs << " is on " <<
 	processor_name << endl;
 
-    n = 10000;			/* default # of rectangles */
+    
     if (myid == 0)
-	startwtime = MPI::Wtime();
-
-    MPI::COMM_WORLD.Bcast(&n, 1, MPI_INT, 0);
-
-    if (myid == 0) {
-	endwtime = MPI::Wtime();
-	cout << "wall clock time = " << endwtime-startwtime << endl;
+	{
+		startwtime = MPI::Wtime();
+		MPI::COMM_WORLD.Send(&test, 1, MPI_INTEGER, 1, 0);
+		test = 2;
+		endwtime = MPI::Wtime();
+		cout << "wall clock time = " << endwtime-startwtime << endl;
     }
+	else if(myid == 1)
+	{
+		test = 0;
+		Sleep(1000);
+		MPI::COMM_WORLD.Recv(&test, 1, MPI_INTEGER, 0, 0);
+
+		cout << "Test = " << test << endl;
+	}
 
     MPI::Finalize();
     return 0;
