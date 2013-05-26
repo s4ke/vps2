d69b83a51ce51962e59214a05422c305976553c1
Georg Rollinger <georg.rollinger@web.de>
2013-05-26 22:04:51 +0200

    3-1  :)

---
+++ vps2-3-1/iprcv.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+const int MAGIC_REQUIRED_BYTES = 12;
+int port = 60000;
+int magic_numbers[3];
+int rcv_sock = 0;
+int stream_sock = 0;
+struct sockaddr_in address;
+unsigned int cli_address_len;
+struct sockaddr_in cli_address;
+char buffer[256];
+int recv_len, magic_len;
+int quit = 0;
+
+
+
+if(argc > 1) {
+   port = atoi(argv[1]);
+}
+
+
+rcv_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+if(rcv_sock < 0) {
+   fprintf(stderr, "Socket creation failed.\n");
+   return -1;
+}
+bzero((char*) &address, sizeof(address));
+address.sin_family = AF_INET;
+address.sin_port = htons(port);
+address.sin_addr.s_addr = INADDR_ANY;
+
+if(bind(rcv_sock, (struct sockaddr *) &address, sizeof(address)) < 0) {
+   fprintf(stderr, "Binding failed.");
+}
+
+listen(rcv_sock, 5);
+fprintf(stderr, "Listening on %i\n", port);
+
+
+while(!quit) {
+	cli_address_len = sizeof(cli_address);
+	stream_sock = accept(rcv_sock, (struct sockaddr *) &cli_address, &cli_address_len);
+	if(stream_sock < 0) {
+	   fprintf(stderr, "Accept failed.\n");
+	} else {
+	    fprintf(stderr, "Accepted.\n");
+	}
+	
+
+	bzero((char*)magic_numbers, MAGIC_REQUIRED_BYTES);
+	magic_len = 0;
+	while(magic_len < MAGIC_REQUIRED_BYTES) {
+	   recv_len = read(stream_sock, (char*)(&magic_numbers[0])+magic_len, MAGIC_REQUIRED_BYTES - magic_len);
+	   if(recv_len < 0) {
+	      fprintf(stderr, "Receiving magic failed.\n");
+	   } else {
+	      magic_len += recv_len;	   
+	   }
+	}
+
+	if(magic_numbers[0] == 21 && magic_numbers[1] == 42 && magic_numbers[2] == 63) {
+		fprintf(stderr, "Right magic.\n");
+		do {
+			bzero(buffer,256);
+			recv_len = read(stream_sock, buffer, 255);
+			if(recv_len < 0) {
+				fprintf(stderr, "Read failed.\n");
+			} else {
+				fprintf(stdout, "%s", buffer);
+			}
+		} while (recv_len > 0);
+		quit = 1;
+		fprintf(stderr, "All Read.\n");
+	} else {
+		fprintf(stderr, "Wrong magic.\n");
+	}
+	close(stream_sock);
+}
+close(rcv_sock);
+
+
+return 0;
+}
---
+++ vps2-3-1/ipsnd.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <netdb.h>
+
+int main(int argc, char** argv)
+{
+const int MAGIC_REQUIRED_BYTES = 12;
+int port = 60000;
+int magic_numbers[3];
+int snd_sock = 0;
+int stream_sock = 0;
+struct sockaddr_in address;
+unsigned int srv_address_len;
+struct sockaddr_in srv_address;
+char buffer[256];
+int recv_len, magic_len;
+int quit = 0;
+char* input_line = NULL;
+struct hostent *server;
+
+if(argc < 3)  {
+   fprintf(stderr, "Too few arguments.\n");
+}
+
+
+
+port = atoi(argv[2]);
+server = gethostbyname(argv[1]);
+if(server == NULL) {
+   fprintf(stderr, "Invalid Server.\n");
+   return -1;
+}
+
+
+snd_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+if(snd_sock < 0) {
+   fprintf(stderr, "Socket creation failed.\n");
+   return -1;
+}
+
+bzero((char*) &srv_address, sizeof(srv_address));
+srv_address.sin_family = AF_INET;
+bcopy((char*)server->h_addr,
+	(char*)&srv_address.sin_addr.s_addr,
+	server->h_length);
+srv_address.sin_port = htons(port);
+
+if(connect(snd_sock, (struct sockaddr *) &srv_address, sizeof(srv_address)) < 0) {
+	fprintf(stderr, "Connection failed.\n");
+	exit(-1);
+}
+
+magic_numbers[0] = 21;
+magic_numbers[1] = 42;
+magic_numbers[2] = 63;
+
+write(snd_sock, magic_numbers, MAGIC_REQUIRED_BYTES);
+
+
+fprintf(stderr, "Start Typing!\n");
+
+
+do {
+	bzero(buffer,256);
+	input_line = fgets(buffer, 255, stdin);
+	if(input_line != NULL) {
+		write(snd_sock, buffer, strlen(buffer));
+	}
+	
+} while (input_line != NULL);
+close(snd_sock);
+
+
+return 0;
+}
